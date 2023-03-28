7718d7e59de08c0c67bbd28499fae1d29e0daefd
Raghav <raghavbhandari082@gmail.com>
2023-03-28 16:53:47 +0530

    Design of webpages completed

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ var check = function() {
     if (document.getElementById('password').value ==
       document.getElementById('r-password').value) {
       document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'matching';
+      document.getElementById('message').innerHTML = 'Password Matching';
     } else {
       document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'not matching';
+      document.getElementById('message').innerHTML = 'Password Not Matching';
     }
 }
+
+// var lengthCheck = function() {
+//   const passLength = document.getElementById("password").value
+//   if (passLength.length<8) {
+//     // if (passLength.length<16) {
+//       document.getElementById("length-check-message").style.color = 'red'
+//       document.getElementById("length-check-message").innerHTML = 'Please Enter the Password Greater than 8 Character'
+//     // }
+//   }
+// }
